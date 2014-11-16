973b8dae5d8859e1cec4103f35fb946bc7bb0712
Raju Gandhi <raju.gandhi+github@gmail.com>
2014-11-15 18:48:17 -0600

    style: fix indentation

--- src/angudone/handler.clj
+++ src/angudone/handler.clj
@@ -19,21 +19,21 @@
 
 (defn init
   "init will be called once when
-   app is deployed as a servlet on
-   an app server such as Tomcat
-   put any initialization code here"
+  app is deployed as a servlet on
+  an app server such as Tomcat
+  put any initialization code here"
   []
   (timbre/set-config!
-    [:appenders :rotor]
-    {:min-level :info
-     :enabled? true
-     :async? false ; should be always false for rotor
-     :max-message-per-msecs nil
-     :fn rotor/appender-fn})
+   [:appenders :rotor]
+   {:min-level :info
+    :enabled? true
+    :async? false ; should be always false for rotor
+    :max-message-per-msecs nil
+    :fn rotor/appender-fn})
 
   (timbre/set-config!
-    [:shared-appender-config :rotor]
-    {:path "angudone.log" :max-size (* 512 1024) :backlog 10})
+   [:shared-appender-config :rotor]
+   {:path "angudone.log" :max-size (* 512 1024) :backlog 10})
 
   (if (env :dev) (parser/cache-off!))
   ;;start the expired session cleanup job
@@ -43,7 +43,7 @@
 
 (defn destroy
   "destroy will be called when your application
-   shuts down, put any clean up code here"
+  shuts down, put any clean up code here"
   []
   (timbre/info "angudone is shutting down...")
   (cronj/shutdown! session-manager/cleanup-job)
@@ -55,21 +55,21 @@
    :timeout-response (redirect "/")})
 
 (defn- mk-defaults
-       "set to true to enable XSS protection"
-       [xss-protection?]
-       (-> site-defaults
-           (update-in [:session] merge session-defaults)
-           (assoc-in [:security :anti-forgery] xss-protection?)))
+  "set to true to enable XSS protection"
+  [xss-protection?]
+  (-> site-defaults
+      (update-in [:session] merge session-defaults)
+      (assoc-in [:security :anti-forgery] xss-protection?)))
 
 (def app (app-handler
-           ;; add your application routes here
-           [home-routes base-routes]
-           ;; add custom middleware here
-           :middleware (load-middleware)
-           :ring-defaults (mk-defaults false)
-           ;; add access rules here
-           :access-rules []
-           ;; serialize/deserialize the following data formats
-           ;; available formats:
-           ;; :json :json-kw :yaml :yaml-kw :edn :yaml-in-html
-           :formats [:json-kw :edn :transit-json]))
+          ;; add your application routes here
+          [home-routes base-routes]
+          ;; add custom middleware here
+          :middleware (load-middleware)
+          :ring-defaults (mk-defaults false)
+          ;; add access rules here
+          :access-rules []
+          ;; serialize/deserialize the following data formats
+          ;; available formats:
+          ;; :json :json-kw :yaml :yaml-kw :edn :yaml-in-html
+          :formats [:json-kw :edn :transit-json]))
