1964fdf38e0f980350fd6b9c65083a529ea47145
Raju Gandhi <raju.gandhi+github@gmail.com>
2014-11-29 13:54:21 -0500

    feat: introduce the todos array

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -4,8 +4,15 @@
   app.controller("TodosCtrl", [
     "$scope",
     function(scope) {
-      var vm = this;
-      vm.description = "This is our first TodosCtrl";
+      var vm = this,
+          index = 0,
+          todos = [
+            { id: ++index, text: "Learn Angular" },
+            { id: ++index, text: "Speak About it" },
+            { id: ++index, text: "Profit!!" }
+          ];
+
+      vm.todos = todos;
     }]);
 
 })(angular.module("TodosApp", []));
