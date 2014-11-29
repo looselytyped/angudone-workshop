cd599b6fee7982a5345d98a6565421b4daf885de
Raju Gandhi <raju.gandhi+github@gmail.com>
2014-11-29 13:27:52 -0500

    feat: write our first (TodosCtrl) controller

--- resources/public/js/main.js
+++ resources/public/js/main.js
@@ -1,1 +1,11 @@
 "use strict";
+
+;(function(app) {
+  app.controller("TodosCtrl", [
+    "$scope",
+    function(scope) {
+      var vm = this;
+      vm.description = "This is our first TodosCtrl";
+    }]);
+
+})(angular.module("TodosApp", []));
